fe127a9ff08c3c5952cb3245fd6fbebc8801ccaf
Qwill Duvall <qwill.duvall@gmail.com>
2017-09-13 16:54:39 -0500

    [1] Q/L - create hello world react app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('./index.css')
+
+class Application extends React.Component {
+  render () {
+    return (
+      <div>
+        Hello World!!!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById('root'))
