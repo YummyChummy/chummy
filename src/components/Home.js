5c513551b61f61a6b35c65c89036365d0853b679
Ling Tran <lxiutran@gmail.com>
2017-09-28 16:58:46 -0500

    WIP story 7

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,21 @@
 var React = require('react');
+var Day = require('./Day');
 
 class Home extends React.Component {
   render(){
     return (
       <div>
           <div className="container">
-              <div className="heading-large">
-                  Make meal planning fun!
+              <div className="intro">
+                  <div className="heading-large">
+                      Make meal planning fun!
+                  </div>
+                  <div className="heading-small">
+                      Start by adding a recipe in the top right. Assign those recipes to days and export into a grocery list.
+                  </div>
               </div>
-              <div className="heading-small">
-                  Start by adding a recipe in the top right. Assign those recipes to days and export into a grocery list.
+              <div className="days-container">
+                <Day />
               </div>
           </div>
       </div>
