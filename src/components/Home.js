60080f1f067d4ee0d62233ec700148d6ea8cc79d
Adam J. Leuer <aleuer2@illinois.edu>
2017-10-03 14:05:20 -0500

    [Tech Task 4] Added date to bottom of page

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,9 @@ class Home extends React.Component {
               <div className="heading-small">
                   Start by adding a recipe in the top right. Assign those recipes to days and export into a grocery list.
               </div>
+              <div style={{position: 'absolute', bottom: '5px'}}>
+                  { new Date().toTimeString() }
+              </div>
           </div>
       </div>
     )
